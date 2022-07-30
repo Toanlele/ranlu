378daef28003cdfcc237fdee8824e9abecfcda11
Toanlele <3143246477@qq.com>
2022-07-31 00:12:18 +0800

    修改参数

--- main.go
+++ main.go
@@ -25,14 +25,8 @@ func main() {
 		panic("mp3.NewDecoder failed: " + err.Error())
 	}
 
-	// Prepare an Oto context (this will use your default audio device) that will
-	// play all our sounds. Its configuration can't be changed later.
+	samplingRate := 34400
 
-	// Usually 44100 or 48000. Other values might cause distortions in Oto
-	samplingRate := 44100
-
-	// Number of channels (aka locations) to play sounds from. Either 1 or 2.
-	// 1 is mono sound, and 2 is stereo (most speakers are stereo).
 	numOfChannels := 2
 
 	// Bytes used by a channel to represent one sample. Either 1 or 2 (usually 2).
@@ -43,29 +37,12 @@ func main() {
 	if err != nil {
 		panic("oto.NewContext failed: " + err.Error())
 	}
-	// It might take a bit for the hardware audio devices to be ready, so we wait on the channel.
 	<-readyChan
-
-	// Create a new 'player' that will handle our sound. Paused by default.
 	player := otoCtx.NewPlayer(decodedMp3)
-
-	// Play starts playing the sound and returns without waiting for it (Play() is async).
 	player.Play()
-
-	// We can wait for the sound to finish playing using something like this
 	for player.IsPlaying() {
 		time.Sleep(time.Millisecond)
 	}
-
-	// Now that the sound finished playing, we can restart from the beginning (or go to any location in the sound) using seek
-	// newPos, err := player.(io.Seeker).Seek(0, io.SeekStart)
-	// if err != nil{
-	//     panic("player.Seek failed: " + err.Error())
-	// }
-	// println("Player is now at position:", newPos)
-	// player.Play()
-
-	// If you don't want the player/sound anymore simply close
 	err = player.Close()
 	if err != nil {
 		panic("player.Close failed: " + err.Error())
